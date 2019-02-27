8dd247b0d4a38dc3b5613953d36944dc7435fe42
Kamilla <kamilla.1201@gmail.com>
2019-02-27 14:12:19 -0800

    Added stroke prediction api call

--- src/routes/predict.js
+++ src/routes/predict.js
@@ -7,10 +7,10 @@ const predictRouter = express.Router()
 predictRouter.use(bodyParser.json())
 
 const project = 'flash-landing-229404'
-const model = 'cervical_cancer'
-const version = 'cervical_cancer3'
-
-const ml_name = `projects/${project}/models/${model}/versions/${version}`
+const ccModel = 'cervical_cancer'
+const ccVersion = 'cervical_cancer3'
+const sModel = 'stroke'
+const sVersion = 'stroke_v2'
 
 function auth (callback) {
   google.auth.getApplicationDefault(function (err, authClient) {
@@ -27,9 +27,8 @@ function auth (callback) {
 }
 
 // Route the HTTP GET request
-predictRouter.post('/predict', function (req, res) {
-  console.log(req.body)
-  var name = req.body.name
+predictRouter.post('/predict/cervical-cancer', function (req, res) {
+  console.log(JSON.stringify(req.body))
   var age = req.body.age
   var partners = req.body.partners
   var intercourse = req.body.intercourse
@@ -52,11 +51,12 @@ predictRouter.post('/predict', function (req, res) {
   var molCont = req.body.molCont
   var hpv = req.body.hpv
   var aids = req.body.aids
+
   auth(function (err, authClient) {
     if (err) {
       console.error(err)
     } else {
-      mongo.insert('cervical_cancer', req.body, (err) => {
+      mongo.insert(ccModel, req.body, (err) => {
         if (err) {
           return res.status(500).json({ message: 'Failed to complete request' })
         }
@@ -65,7 +65,7 @@ predictRouter.post('/predict', function (req, res) {
           auth: authClient
         })
         ml.projects.predict({
-          name: ml_name,
+          name: `projects/${project}/models/${ccModel}/versions/${ccVersion}`,
           resource: {
             instances: [[age, partners, intercourse, pregnancies, smokes, smokePacks, contraceptives, iud, stds, stdsNum, condyl, vCondyl, pid, hiv, cCondyl, vpc, gHerpes, hepB, syphilis, molCont, hpv, aids]]
           }
@@ -73,6 +73,56 @@ predictRouter.post('/predict', function (req, res) {
           if (err) {
             console.error('ERROR PRIORITY', err)
           }
+          console.log('RES: ', JSON.stringify(result))
+          if (result && result.data && result.data.predictions) {
+            res.status(200).json(result.data.predictions[0])
+          }
+          return res.status(500).json({ message: 'Something went wrong.' })
+        })
+      })
+    }
+  })
+})
+
+// Route the HTTP GET request
+predictRouter.post('/predict/stroke', function (req, res) {
+  console.log(JSON.stringify(req.body))
+  var age = req.body.age
+  var gender = req.body.gender
+  var hypertension = req.body.hypertension
+  var heartDisease = req.body.heartDisease
+  var everMarried = req.body.everMarried
+  var workType = req.body.workType
+  var residence = req.body.residence
+  var hFeet = req.body.hFeet
+  var hInches = req.body.hInches
+  var wPounds = req.body.wPounds
+
+  var inches = hFeet * 12 + hInches
+  var bmi = Math.round(703 * wPounds / Math.pow(inches, 2))
+
+  auth(function (err, authClient) {
+    if (err) {
+      console.error(err)
+    } else {
+      mongo.insert(sModel, req.body, (err) => {
+        if (err) {
+          return res.status(500).json({ message: 'Failed to complete request' })
+        }
+        var ml = google.ml({
+          version: 'v1',
+          auth: authClient
+        })
+        ml.projects.predict({
+          name: `projects/${project}/models/${sModel}/versions/${sVersion}`,
+          resource: {
+            instances: [[age, gender, hypertension, heartDisease, everMarried, workType, residence, bmi]]
+          }
+        }, function (err, result) {
+          if (err) {
+            console.error('ERROR PRIORITY', err)
+          }
+          console.log('RES: ', JSON.stringify(result))
           if (result && result.data && result.data.predictions) {
             res.status(200).json(result.data.predictions[0])
           }
